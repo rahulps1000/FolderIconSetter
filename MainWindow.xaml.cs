5b17f2261b16ceda9fc81343d8db2459e38cac4b
Rahul P S <rahulps1000@gmail.com>
2024-04-23 18:14:13 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using FolderIconSetter.Models;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FolderIconSetter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private IconSetter iconSetter;
+        public MainWindow()
+        {
+            InitializeComponent();
+            iconSetter = new IconSetter();
+            this.DataContext = iconSetter;
+        }
+
+
+        private void titleBarClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void closeApp(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void openFolderSelector(object sender, RoutedEventArgs e)
+        {
+            iconSetter.FolderSelector();
+        }
+
+        private void openIconSelector(object sender, RoutedEventArgs e)
+        {
+            iconSetter.IconSelector();
+        }
+
+        private void openGithub(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                Process.Start("https://github.com/rahulps1000/FolderIconSetter");
+            }
+        }
+    }
+}
---
+++ Models/IconSetter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using FolderIconSetter.Core;
+
+namespace FolderIconSetter.Models
+{
+    internal class IconSetter : ObservableObject
+    {
+
+        private String folderPath;
+        private String _iconPath;
+
+        public String FolderPath
+        {
+            get { return folderPath; }
+            set {
+                folderPath = value; 
+                OnProprtyChanged();
+            }
+        }
+
+        public String IconPath
+        {   
+            get { return _iconPath; }
+            set {
+                _iconPath = value; 
+                OnProprtyChanged();
+            }
+        }
+
+        public IconSetter()
+        {
+            FolderPath = String.Empty;
+            IconPath = String.Empty;
+        }
+
+        public void FolderSelector ()
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Select a folder"
+            };
+
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                FolderPath = dialog.FileName;
+            }
+
+        }
+
+        public void IconSelector ()
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                Title = "Select an ICO File"
+            };
+
+            dialog.Filters.Add(new CommonFileDialogFilter("ICO Files", "*.ico"));
+
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                string filePath = dialog.FileName;
+                if (System.IO.Path.GetExtension(filePath).ToLower() == ".ico")
+                {
+                    IconPath = filePath;
+                }
+            }
+        }
+
+        public void SetIcon()
+        {
+
+        }
+    }
+}
